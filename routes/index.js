48ed10787c20521874e3e942a12ad588c3a4dde9
Screen Goose <sarrieschaz@hotmail.com>
2015-04-30 18:47:52 +0800

    Pre-loaded objects

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
 
 /* GET home page. */
@@ -7,12 +10,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-
-
-var mongoose = require('mongoose');
-var Post = mongoose.model('Post');
-var Comment = mongoose.model('Comment');
 
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
@@ -30,6 +27,22 @@ router.post('/posts', function(req, res, next) {
 
     res.json(post);
   });
+});
+
+router.param('post', function(req, res, next) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if(err){ return next(err);}
+    if(!post){ return next(new Error('can\'t find post'));}
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res){
+  res.json(req.post);
 });
 
 
